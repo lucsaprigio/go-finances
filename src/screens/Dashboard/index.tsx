207a0d38d6b9e69874fc721a909c94040d8b67c6
Lucas Aprigio <lucsaprigio@hotmail.com>
2021-07-27 07:52:44 -0300

    Created Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import { 
+    Container, 
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName, 
+    UserWrapper,
+} from './styles';
 
 export function Dashboard(){
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                <UserInfo>
+                    <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/68642596?v=4'}} />
+
+                    <User>
+                        <UserGreeting>Olá,</UserGreeting>
+                        <UserName>Lucas</UserName>
+                    </User>
+
+                </UserInfo>
+            </UserWrapper>
+            </Header>
         </Container>
     )
 }
